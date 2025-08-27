df632437f65a040d759012c1329c265608cc7bc2
anujan542 <anujan9300@gmail.com>
2025-08-27 14:39:57 +0530

    Updated path

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  base: process.env.VITE_BASE_PATH || '/e-invyt-client',
+  base: process.env.VITE_BASE_PATH || '/',
 });
